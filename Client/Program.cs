877cdb48fd81f721218a1a20cb54e29e6ab4194a
GLuca <GLuca@DESKTOP-6Q8KE4E>
2024-01-02 19:46:22 +0100

    Aggiungere i file di progetto.

---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Client
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			const string file = @"search.jpg";
+			const string URL = "https://localhost:7071/fileupload/UploadLargeFile";
+			await Send(URL, file);
+			
+			const string file2 = @"search2.jpg";
+			const string URL2 = "https://localhost:7071/fileupload/UploadLargeFileCancel";
+			await Send(URL2, file2);
+		}
+
+
+
+
+		private static async Task Send(string URL, params string[] files)
+		{
+			MultipartFormDataContent multipartContent = new MultipartFormDataContent();
+
+			foreach (string file in files)
+			{
+				var streamContent = new StreamContent(File.Open(file, FileMode.Open));
+				multipartContent.Add(streamContent, Path.GetFileNameWithoutExtension(file));
+			}
+			HttpClient httpClient = new HttpClient();
+			HttpResponseMessage response = await httpClient.PostAsync(URL, multipartContent,new CancellationToken());
+
+			response.EnsureSuccessStatusCode();
+
+		}
+
+
+	}
+}
---
+++ ErrorMultipart/Controllers/FileUploadController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Net;
+
+namespace MetropolisNetwork.Metropolis.Tests.WebApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	[DisableFormValueModelBindingAttribute]
+	public class FileUploadController : ControllerBase
+	{
+		// Get the default form options so that we can use them to set the default 
+		// limits for request body data.
+		private static readonly FormOptions _defaultFormOptions = new FormOptions();
+		private readonly long _fileSizeLimit;
+		private readonly string[] _permittedExtensions = { ".txt" };
+		private readonly ILogger<FileUploadController> _logger;
+		private readonly string _targetFilePath;
+
+		public FileUploadController(ILogger<FileUploadController> logger, IConfiguration config)
+		{
+			_fileSizeLimit = 100;// config.GetValue<long>("FileSizeLimit");
+
+			// To save physical files to a path provided by configuration:
+			_targetFilePath = @"D:\VM\Temp";// config.GetValue<string>("StoredFilesPath")!;
+			_logger = logger;
+		}
+
+
+		[HttpPost]
+		[Route(nameof(UploadLargeFile))]
+		[DisableRequestSizeLimitAttribute()]
+		[DisableFormValueModelBindingAttribute]
+		public async Task<IActionResult> UploadLargeFile()
+		{
+			var boundary = Request.GetMultipartBoundary();
+			var reader = new MultipartReader(boundary, Request.Body);
+			var section = await reader.ReadNextSectionAsync();
+			while (section != null)
+				section = await reader.ReadNextSectionAsync();
+
+			return Ok();
+		}
+
+		[HttpPost]
+		[Route(nameof(UploadLargeFileCancel))]
+		[DisableRequestSizeLimitAttribute()]
+		[DisableFormValueModelBindingAttribute]
+		public async Task<IActionResult> UploadLargeFileCancel(CancellationToken cancellationToken)
+		{
+			var boundary = Request.GetMultipartBoundary();
+			var reader = new MultipartReader(boundary, Request.Body);
+			var section = await reader.ReadNextSectionAsync();
+			while (section != null)
+				section = await reader.ReadNextSectionAsync();
+			return Ok();
+		}
+
+
+
+
+	}
+
+
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
+	{
+		public void OnResourceExecuting(ResourceExecutingContext context)
+		{
+			var factories = context.ValueProviderFactories;
+			factories.RemoveType<FormValueProviderFactory>();
+			factories.RemoveType<JQueryFormValueProviderFactory>();
+		}
+
+		public void OnResourceExecuted(ResourceExecutedContext context)
+		{
+		}
+	}
+}
